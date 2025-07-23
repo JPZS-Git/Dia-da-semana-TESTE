3641ed3404e29f118ea10e652186ed3bc4f2be3c
Jp_zs <joaopedrosartori2016@gmail.com>
2025-07-23 15:00:58 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Write("Digite um número de 1 a 7: ");
+        string entrada = Console.ReadLine();
+        
+        if (int.TryParse(entrada, out int numero))
+{
+    switch (numero)
+    {
+        case 1:
+            Console.WriteLine("Domingo");
+            break;
+        case 2:
+            Console.WriteLine("Segunda-feira");
+            break;
+        case 3:
+            Console.WriteLine("Terça-feira");
+            break;
+        case 4:
+            Console.WriteLine("Quarta-feira");
+            break;
+        case 5:
+            Console.WriteLine("Quinta-feira");
+            break;
+        case 6:
+            Console.WriteLine("Sexta-feira");
+            break;
+        case 7:
+            Console.WriteLine("Sábado");
+            break;
+        default:
+            Console.WriteLine("Erro: número fora do intervalo de 1 a 7.");
+            break;
+    }
+}
+else
+{
+    Console.WriteLine("Erro: entrada inválida. Por favor, digite um número inteiro.");
+}
+    
